8c50508212c7a4ae84fa6acfbeba0d8ab5d9e6d8
VaradSinghal <varadsinghal1526@gmail.com>
2024-11-23 19:17:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+});
+
+document.querySelectorAll('.hidden').forEach(element => {
+  observer.observe(element);
+});
+
